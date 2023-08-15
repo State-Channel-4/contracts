bbbe415450039830c0d69268691b5781f2eca84a
Nico Serrano <nnicosp@hotmail.com>
2023-08-15 00:35:33 -0500

    test(feat): WIP successfully submit content

--- test/create.ts
+++ test/create.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import { createContentIfNotExistsFixture } from "./fixtures";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { firstTag, secondTag } from "../constants";
 
 
 describe("Create", async function () {
@@ -9,16 +10,27 @@ describe("Create", async function () {
 
       const allContent = await channel4Contract.getAllContent();
       const allTags = await channel4Contract.getAllTags();
-      const firstTagObject = allTags[0];
+
       expect( allContent.length ).to.equal(2); // remember that a content is added in deploy and other in create
-      expect( allTags.length ).to.equal(contentObj.tags.length); // keep in mind that firstTag is included
+      expect( allTags.length ).to.equal(contentObj.tags.length); // remember that firstTag is included in content.tags
 
-      const contentIdsInFirstTag = firstTagObject.contentIds;
+      const content = await channel4Contract.getContent(contentObj.url);
 
-      expect( contentIdsInFirstTag.length ).to.equal(1);
-      expect( contentIdsInFirstTag[0] ).to.equal(allContent.length); // check for content ids
+      expect( content.title ).to.equal(contentObj.title);
+      expect( content.url ).to.equal(contentObj.url);
+      expect( content.submittedBy ).to.equal(contentObj.submittedBy);
+      expect( Number(content.likes) ).to.equal(0);
+      expect( content.tagIds.length ).to.equal(contentObj.tags.length);
+
+      content.tagIds.forEach( (tag) => {
+        const tagObj = allTags[Number(tag)];
+        console.log(tagObj)
+        expect([firstTag, secondTag]).to.include(tagObj.name);
+        //expect( tagObj.contentIds.length ).to.equal(1);
+      });
     });
 
+    /*
     it("Should have the correct Content object attributes", async function () {
       const { channel4Contract, otherAccount1, contentObj } = await loadFixture(createContentIfNotExistsFixture);
 
@@ -41,5 +53,6 @@ describe("Create", async function () {
       const allTags = await channel4Contract.getAllTags();
       expect( allTags.length ).to.equal(contentObj.tags.length);
     });
+    */
 
   });
