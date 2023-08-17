74baca19b0cc5a2977b9b29f3495aae3c423389f
Nico Serrano <nnicosp@hotmail.com>
2023-08-17 17:18:52 -0500

    contract(docs): delete unused comment

--- contracts/Channel4Contract.sol
+++ contracts/Channel4Contract.sol
@@ -8,12 +8,6 @@ import { Interact } from "./Interact.sol";
 import { Litigate } from "./Litigate.sol";
 
 contract Channel4Contract is Data, Create, Interact, Litigate {
-
-    /// @notice Initialize contract with one tag and one content
-    /// @dev Initialize with one element in each list to prevent the mapping of initial tag (0) to be confused as a empty value
-    /// @param title First content title
-    /// @param url First content link
-    /// @param tag First tag name
     constructor (
         string memory title,
         string memory url,
