6b0861a202c285729287fda15911d802a9e193a8
Nico Serrano <nnicosp@hotmail.com>
2023-09-08 14:40:12 -0400

    litigate(feat): litigate tag added

--- contracts/Litigate.sol
+++ contracts/Litigate.sol
@@ -12,6 +12,9 @@ abstract contract Litigate is Data, Create, Slasher, EIP712 {
     bytes32 private constant CONTENT_TO_ADD_TYPE =
     keccak256("ContentToAdd(string title,string url,address submittedBy,uint256 likes,string[] tagIds)");
 
+    bytes32 private constant TAG_TO_ADD_TYPE =
+    keccak256("TagToAdd(string name,address createdBy,string[] contentIds)");
+
     constructor(uint256 slashingFee, uint256 backendRegistrationFee)
         EIP712("Channel4Contract", "0.0.1")
         Slasher(slashingFee, backendRegistrationFee)
@@ -19,11 +22,11 @@ abstract contract Litigate is Data, Create, Slasher, EIP712 {
 
     /// Ligitation functions
 
-    /// @notice Verify if EIP-712 signature is valid
+    /// @notice Verify if EIP-712 signature for content is valid
     /// @dev For EIP-712 in Solidity check https://gist.github.com/markodayan/e05f524b915f129c4f8500df816a369b
     /// @param content Content message
     /// @param signature EIP-712 signature
-    function verifyMetaTx(ContentToAdd calldata content, bytes calldata signature) public view returns (bool) {
+    function verifyMetaTxContent(ContentToAdd calldata content, bytes calldata signature) public view returns (bool) {
         bytes32[] memory encodedTagIds = new bytes32[](content.tagIds.length);
         for (uint256 i = 0; i < content.tagIds.length; i++) {
             encodedTagIds[i] = keccak256(bytes(content.tagIds[i]));
@@ -41,6 +44,26 @@ abstract contract Litigate is Data, Create, Slasher, EIP712 {
         return signer == backendAddress;
     }
 
+    /// @notice Verify if EIP-712 signature for tag is valid
+    /// @dev For EIP-712 in Solidity check https://gist.github.com/markodayan/e05f524b915f129c4f8500df816a369b
+    /// @param tag Tag message
+    /// @param signature EIP-712 signature
+    function verifyMetaTxTag(TagToAdd calldata tag, bytes calldata signature) public view returns (bool) {
+        bytes32[] memory encodedContentIds = new bytes32[](tag.contentIds.length);
+        for (uint256 i = 0; i< tag.contentIds.length; i++){
+            encodedContentIds[i] = keccak256(bytes(tag.contentIds[i]));
+        }
+
+        bytes32 digest = _hashTypedDataV4(keccak256(abi.encode(
+            TAG_TO_ADD_TYPE,
+            keccak256(bytes(tag.name)),
+            tag.createdBy,
+            keccak256(abi.encodePacked( encodedContentIds ))
+        )));
+        address signer = ECDSA.recover(digest, signature);
+        return signer == backendAddress;
+    }
+
     /// @notice Litigate a specific content (add it and claim slashing)
     /// @dev For more info about verifying EIP-712
     /// check https://github.com/aglawson/Meta-Transactions/blob/main/contracts/MinimalForwarder.sol
@@ -50,7 +73,7 @@ abstract contract Litigate is Data, Create, Slasher, EIP712 {
         ContentToAdd calldata content,
         bytes calldata signature
     ) public returns (bool) {
-        require( verifyMetaTx(content, signature), "Invalid signature");
+        require( verifyMetaTxContent(content, signature), "Invalid signature");
         // check if content is already registered
         string memory firstUrl = contents.list[0].url;
         require( keccak256(bytes(content.url)) != keccak256(bytes(firstUrl)), "Initial content is not litigable" );
@@ -71,18 +94,51 @@ abstract contract Litigate is Data, Create, Slasher, EIP712 {
                     isTagIdsCorrect = isTagIdsCorrect && isTagNameCorrect;
                 }
             }
-            // if Content values are correct return true
+            // if Content values are correct return false because litigation is not correct
             if (isTitleCorrect && isSubmittedByCorrect && isTagIdsCorrect){
                 return false;
             }
         }
-        // content does not exists or values are incorrect, add it
+        // content does not exists or values are incorrect, add it or modify it
         _createContentIfNotExists(
             content.title,
             content.url,
             content.submittedBy,
             content.likes,
             content.tagIds
+        );
+        slashBackend(msg.sender);
+        return true;
+    }
+
+    /// @notice Litigate a specific tag (add it and claim slashing)
+    /// @dev For more info about verifying EIP-712
+    /// check https://github.com/aglawson/Meta-Transactions/blob/main/contracts/MinimalForwarder.sol
+    /// @param tag Tag to litigate
+    /// @param signature EIP-712 signature
+    function litigateTag(
+        TagToAdd calldata tag,
+        bytes calldata signature
+    ) public returns (bool) {
+        require( verifyMetaTxTag(tag, signature), "Invalid signature" );
+        // check if tag is already registered
+        string memory firstTag = tags.list[0].name;
+        require( keccak256(bytes(tag.name)) != keccak256(bytes(firstTag)), "Initial tag is not litigable" );
+        uint256 tagIndex = tags.ids[tag.name];
+        if (tagIndex != 0){
+            // tag exists, check if constant values are correct
+            Tag memory existingTag = tags.list[tagIndex];
+            bool isNameCorrect = keccak256(bytes(tag.name)) == keccak256(bytes(existingTag.name));
+            bool isCreatedByCorrect = tag.createdBy == existingTag.createdBy;
+            // if tag values are correct return false because litigation is not correct
+            if (isNameCorrect && isCreatedByCorrect){
+                return false;
+            }
+        }
+        // tag does not exists or values are incorrect, add it or modify it
+        _createTagIfNotExists(
+            tag.name,
+            tag.createdBy
         );
         slashBackend(msg.sender);
         return true;
