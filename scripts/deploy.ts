0c52927635fab7fcc98402269dbd489186a7c5f4
Jack Gilcrest <me@jp4g.llc>
2023-09-17 19:52:15 -0600

    fix: update deploy script to use new contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,38 @@
 import { ethers } from 'hardhat';
-import { FIRST_TAG } from '../constants';
-
+import {
+  BACKEND_PRIVATE_KEY,
+  BACKEND_REGISTRATION_FEE,
+  FIRST_TAG,
+  FIRST_TITLE,
+  FIRST_URL,
+  SLASHING_FEE,
+} from '../constants';
 async function main() {
-  const UrlContract = await ethers.getContractFactory('UrlContract');
-  const urlContract = await UrlContract.deploy(FIRST_TAG);
-
-  await urlContract.deployed();
-
-  console.log(
-    `UrlContract deployed to ${urlContract.address} with first tag ${FIRST_TAG}`,
+  const network = await ethers.provider.getNetwork();
+  console.log(`Deploying Channel4Contract to ${network.name} (${network.chainId})...`)
+  
+  // deploy contract
+  const Channel4Contract = await ethers.getContractFactory('Channel4Contract');
+  const channel4Contract = await Channel4Contract.deploy(
+    FIRST_TITLE,
+    FIRST_URL,
+    FIRST_TAG,
+    SLASHING_FEE,
+    BACKEND_REGISTRATION_FEE,
   );
+  await channel4Contract.waitForDeployment();
+
+  // log deployment info
+  console.log(`====================================`);
+  console.log(`Channel4Contract deployed to '${await channel4Contract.getAddress()}'`);
+  console.log(`Deployment parameters: `);
+  console.log(` - First URL Title: "${FIRST_TITLE}"`);
+  console.log(` - First URL: "${FIRST_URL}"`);
+  console.log(` - First Tag: "${FIRST_TAG}"`);
+  console.log(` - Slashing Fee: ${ethers.formatEther(SLASHING_FEE)} ether`);
+  console.log(` - Backend Registration Fee: ${ethers.formatEther(BACKEND_REGISTRATION_FEE)} ether`);
+  console.log(`====================================`);
+  // @todo: etherscan verification
 }
 
 // We recommend this pattern to be able to use async/await everywhere
